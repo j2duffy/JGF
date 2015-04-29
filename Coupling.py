c7235e6f82af3d3eb9cbe8cd5f5d2067970efcbf
duffyj2 <duffyj2@tcd.ie>
2015-04-29 18:30:46 +0100

    Added the coupling code for nanotubes

--- Coupling.py
+++ Coupling.py
@@ -315,6 +315,17 @@ def CenterGen2(rij,E):
   return g_mx
 
 
+def JGNRTubeSubs(nC,m,n,s):
+  """A routine for calculating the coupling for substitutional impurities in a nanotube. Really not tested a whole bunch."""
+  def GF(y):
+    return gTubeSubsMx(nC,m,n,s,EF+1j*y)
+  def integrand(y):
+    return 1.0/pi*log( abs(1.0 + ex_split**2 * Dyson(GF(y),Vup)[1,0] * Dyson(GF(y),Vdown)[0,1])  ).real
+  C = quad(integrand, eta, np.inf, epsabs=0.0e0, epsrel=1.0e-4, limit=200 ) 
+  return C[0]
+
 if __name__ == "__main__":
-  nE,m1,n1,m2,n2,s = 6,1,0,5,3,0
-  print JGNRTop(nE,m1,n1,m2,n2,s)
+  nC,s = 21,0
+  for m in range(10,50):
+    n = -m
+    print m, JGNRTubeSubs(nC,m,n,s)
