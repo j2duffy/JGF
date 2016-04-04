042142692687ece649af5f4b943e6daf49439197
duffyj2 <duffyj2@tcd.ie>
2016-04-04 14:05:38 +0100

    Changed the Structure of data files a bit. Some other changes

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 # Currently working on some shit with my Dynamic code
 from Dynamic import *
 
-def plot(nE, DZ,interval):
+def Xplot(nE,DZ,interval):
   Vup, Vdown = SC1GNRTop(nE,DZ,0)
   fXi = np.vectorize(lambda w: X1RPAGNRTop(nE,DZ,0,Vup,Vdown,w).imag)
   wilist, Xitemp = sample_function(fXi,interval, tol=1e-3)
   Xilist = Xitemp[0]
   return wilist, Xilist
 
-def humanplot(nE, DZ):
+def AdjustPlot(nE,DZ):
   # Initial plot
-  w,X = plot(nE, DZ,[0,1.0e-2])
+  w,X = Xplot(nE,DZ,[0,1.0e-2])
   pl.plot(w,X)
   pl.show()
-  # Should start your loop here
+  # Until you give the OK
   while True:
     # Get Center
     imin = X.argmin()
@@ -21,32 +21,30 @@ def humanplot(nE, DZ):
     #Request range
     wrange = eval(raw_input("Select a range:"))
     # Replot with new range 
-    w,X = plot(nE, DZ,[wmin-wrange,wmin+wrange])
+    w,X = Xplot(nE,DZ,[wmin-wrange,wmin+wrange])
     pl.plot(w,X)
     pl.show()
     # If range works out, save and exit
-    text = raw_input("Press enter to iterate, type 'save' to save:")
-    if text != "":
-      np.savetxt("X_nE%i_DZ%i.dat" % (nE,DZ) ,(w,X))
+    text = raw_input("Save plot? (y or n):")
+    if text == "y":
+      np.savetxt("data/X_nE%i_DZ%i.dat" % (nE,DZ) ,(w,X))
       return
     
 if __name__ == "__main__":   
-  #nE,DZ = 12,1
-  #Vup, Vdown = SC1GNRTop(nE,DZ,0)
-  #fXi = np.vectorize(lambda w: X1RPAGNRTop(nE,DZ,0,Vup,Vdown,w).imag)
-  #wilist, Xitemp = sample_function(fXi,[0,1.0e-2], tol=1e-3)
-  #Xilist = Xitemp[0]
-  #np.savetxt("X_nE%i_DZ%i.dat" % (nE,DZ) ,(wilist,Xilist))
-  #pl.plot(wilist,Xilist)
-  #pl.show()
-  
-  # Until decided
-  nE = 12
-  DZlist = range(1,nE)
-  for DZ in DZlist:
-    humanplot(nE, DZ)
+  nE,DZ = 6,1
+  Vup, Vdown = SC1GNRTop(nE,DZ,0,1.4)
+  fXi = np.vectorize(lambda w: X1RPAGNRTop(nE,DZ,0,Vup,Vdown,w).imag)
+  wilist, Xitemp = sample_function(fXi,[0,1.0e-2],tol=1e-3)
+  Xilist = Xitemp[0]
+  pl.plot(wilist,Xilist)
+  pl.show()
 
-  #nE = 9
+  #nE = 30
+  #DZlist = range(1,nE)
+  #for DZ in DZlist:
+    #AdjustPlot(nE, DZ)
+    
+  #nE = 30
   #minlist = []
   #DZlist = range(1,nE)
   #for DZ in DZlist:
@@ -54,9 +52,9 @@ if __name__ == "__main__":
       #DZlist.remove(DZ)
       
   #for DZ in DZlist:
-    #w,X = np.loadtxt("X_nE%i_DZ%i.dat" % (nE,DZ))
+    #w,X = np.loadtxt("data/X_nE%i_DZ%i.dat" % (nE,DZ))
     #minlist.append(X.min())
     
-  #pl.plot(DZlist,minlist,'o')
+  #pl.plot(DZlist,minlist)
   #pl.savefig("test.png")
   #pl.show()
