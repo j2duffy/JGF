99f6d81c1d69ce107ec54b7b34299050af36a1c1
duffyj2 <duffyj2@tcd.ie>
2016-04-22 15:03:14 +0100

    Final work commit

--- Main.py
+++ Main.py
@@ -80,23 +80,31 @@ def PeakHeight():
   pl.show()
   
   
-if __name__ == "__main__":  
-
+if __name__ == "__main__":    
+  #nE = 30
+  #DZlist = [DZ for DZ in range(1,nE) if DZ % 3 != 0]
+  #FWHMlist = []
+  #for DZ in DZlist:
+    #w,X = np.loadtxt("data/X_nE%i_DZ%i_EF0.0.dat" % (nE,DZ))
+    #X = X - X.min()/2.0
+    #spline = UnivariateSpline(w,X,s=0.1)
+    #r1, r2 = spline.roots() # find the roots
+    #FWHMlist.append(r2 - r1)
+    #pl.plot(w,X)
+    #pl.plot(w,spline(w))
+    #pl.show()
+  #pl.plot(DZlist,FWHMlist,'-o')
+  #pl.show()
   
-  nE = 6
-  DZ = 3
-  EFlist = np.linspace(0,1.1,12)
-  EFlist = EFlist[EFlist != 1]
-  FWHMlist = []
-  for EF in EFlist:
-    w,X = np.loadtxt("data/X_nE%i_DZ%i_EF%.1f.dat" % (nE,DZ,EF))
-    X = X - X.min()/2.0
-    spline = UnivariateSpline(w,X,s=0.1)
-    r1, r2 = spline.roots() # find the roots
-    FWHMlist.append(r2 - r1)
-  pl.plot(EFlist,FWHMlist)
-  np.savetxt("FWHM2.dat",(EFlist,FWHMlist))
-  pl.show()
+  #nE = 30
+  #DZlist = [DZ for DZ in range(1,nE) if DZ % 3 != 0]
+  #Ph = []
+  #for DZ in DZlist:
+    #w,X = np.loadtxt("data/X_nE%i_DZ%i_EF0.0.dat" % (nE,DZ))
+    #Ph.append(X.min())
+  #pl.plot(DZlist,Ph,'-o')
+  #pl.show()
+
 
 
 
--- NicePlot.py
+++ NicePlot.py
@@ -3,7 +3,41 @@ import pylab as pl
 import seaborn as sns
 from Dynamic import *
 
-if __name__ == "__main__":   
+def X1plot():
+  sns.set(font_scale=1.5)
+  
+  pl.figure(figsize=(12,6))
+  
+  pl.subplot(1,2,1)
+  w,X = np.loadtxt("X1DZ1re.dat").T
+  pl.plot(w,X)
+  w,X = np.loadtxt("X1DZ1im.dat").T
+  pl.plot(w,X)
+  pl.xlim([0.85e-3,1.1e-3])
+  
+  pl.xlabel(r"$\omega$")
+  pl.ylabel(r"$\chi(\omega)$")
+  
+  pl.ticklabel_format(style='sci',scilimits=(0,0))
+  
+  pl.subplot(1,2,2)
+  w,X = np.loadtxt("X1DZ3re.dat").T
+  pl.plot(w,X)
+  w,X = np.loadtxt("X1DZ3im.dat").T
+  pl.plot(w,X)
+  pl.xlim([9.694658e-4,9.694661e-4])
+  
+  pl.xlabel(r"$\omega$")
+  pl.ylabel(r"$\chi(\omega)$")
+  
+  pl.ticklabel_format(style='sci',scilimits=(0,0))
+  
+  pl.tight_layout()
+  pl.savefig("X1.pdf")
+  pl.show()
+  
+  
+def FWHMvsDZplot():
   sns.set(font_scale=1.5)
   
   DZ, FWHM = np.loadtxt("PaperData/FWHMvDZ.dat")
@@ -13,16 +47,10 @@ if __name__ == "__main__":
   pl.tight_layout()
   pl.savefig("X1FWHM.pdf")
   pl.show()
-  
-  #EFlist, FWHM = np.loadtxt("PaperData/FWHM.dat")
-  #pl.plot(EFlist, FWHM, '-o')
-  #pl.xlabel(r"$E_F$")
-  #pl.ylabel(r"$FWHM(E_F)$")
-  #pl.ticklabel_format(style='sci',scilimits=(0,0))
-  #pl.tight_layout()
-  #pl.savefig("test.png")
-  #pl.show()
-  
+
+if __name__ == "__main__": 
+  sns.set(font_scale=1.5)
+
   #EFlist = [0.0,0.5,0.7,0.8,0.9]
   #for EF in EFlist:
     #w,X = np.loadtxt("data/X_nE6_DZ1_EF%.1f.dat" % (EF,))
@@ -41,32 +69,21 @@ if __name__ == "__main__":
   #pl.show()
 
   
-  #EFlist,FWHM = np.loadtxt("PaperData/FWHM.dat")
-  #pl.plot(EFlist,FWHM)
+  EFlist,FWHM = np.loadtxt("PaperData/FWHM2vEF_DZ3.dat")
+  pl.plot(EFlist,FWHM,'-o')
   
-  #pl.xlabel(r"$E_F$")
-  #pl.ylabel(r"$FWHM(E_F)$")
+  pl.xlabel(r"$E_F$")
+  pl.ylabel(r"$FWHM(E_F)$")
   
-  #pl.ticklabel_format(style='sci',scilimits=(0,0))
+  pl.ticklabel_format(style='sci',scilimits=(0,0))
   
-  #pl.axes([0.15,0.5,0.4,0.4])
-  #x,y = np.loadtxt("PaperData/DOS_DZ3.dat")
-  #pl.plot(x,y)
+  pl.axes([0.15,0.5,0.4,0.4])
+  x,y = np.loadtxt("PaperData/DOS_DZ3.dat")
+  pl.plot(x,y)
   
-  #pl.tick_params(labelbottom=False,labelleft=False) # labels along the bottom edge are off
+  pl.tick_params(labelbottom=False,labelleft=False) # labels along the bottom edge are off
   
-  #pl.tight_layout()
+  pl.tight_layout()
   
-  #pl.savefig("VLDOS_FWHM.pdf")
-  #pl.show()
-    
-
-  
-  
-
-
-  
-
-  
-  #pl.savefig("test.png")
-  #pl.show()
+  pl.savefig("FWHMvEF_DZ3.pdf")
+  pl.show()
